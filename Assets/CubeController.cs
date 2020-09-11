524fc4d36f8cb489cf497d2a7ab73e953a17d9e0
shingo2232-droid <shingo.kanda@gmail.com>
2020-09-11 21:19:16 +0900

    課題7提出

---
+++ Assets/CubeController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+    private AudioSource audioSource;
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "field"|| other.gameObject.tag=="CubeTag")
+        {
+            AudioSource source = gameObject.GetComponent<AudioSource>();
+            source.Play();
+        }
+      
+     }
+}
